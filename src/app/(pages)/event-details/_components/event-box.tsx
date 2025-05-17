f1640b497072e707b545e7743352ab31cacf1ee1
premkumar <premkumarananthan1@gmail.com>
2025-05-17 14:17:34 +0530

    changes

--- src/app/(pages)/event-details/_components/event-box.tsx
+++ src/app/(pages)/event-details/_components/event-box.tsx
@@ -1,33 +1,45 @@
 import SocialLinks from '@/components/social/social-links';
 import { CalenderTwoSvg,LocationThree } from '@/components/svg';
+import { IEventDT } from '@/types/event-d-t';
+interface EventBoxProps {
+  event: IEventDT;
+}
 
-export default function EventBox({event}:any) {
+export default function EventBox({ event }: EventBoxProps) {
   return (
     <div className="tp-event-details-box">
-        <div className="tp-event-details-details">
-            <h4 className="tp-event-details-box-title">Event Detail</h4>
-            <div className="tp-event-details-list d-flex align-items-center justify-content-between">
-                <h5><span>
-                <CalenderTwoSvg/>
-                    </span> Date</h5>
-                <span>{event?.acf?.date}</span>
-            </div>
-            
-            <div className="tp-event-details-list d-flex align-items-center justify-content-between">
-                <h5><span><LocationThree/></span> Location</h5>
-                <span>{event?.acf?.location}</span>
-            </div>
+      <div className="tp-event-details-details">
+        <h4 className="tp-event-details-box-title">Event Detail</h4>
+        <div className="tp-event-details-list d-flex align-items-center justify-content-between">
+          <h5>
+            <span>
+              <CalenderTwoSvg />
+            </span>{" "}
+            Date
+          </h5>
+          <span>{event?.acf?.date}</span>
         </div>
-        <div className="tp-event-details-ticket">
-           
-            
-            <div className="tp-event-details-btn mb-30">
-                <a className="tp-btn-inner w-100 text-center" href="#">Share This Event</a>
-            </div>
-            <div className="tp-event-details-social text-center">
-                <SocialLinks/>
-            </div>
+
+        <div className="tp-event-details-list d-flex align-items-center justify-content-between">
+          <h5>
+            <span>
+              <LocationThree />
+            </span>{" "}
+            Location
+          </h5>
+          <span>{event?.acf?.location}</span>
         </div>
+      </div>
+      <div className="tp-event-details-ticket">
+        <div className="tp-event-details-btn mb-30">
+          <a className="tp-btn-inner w-100 text-center" href="#">
+            Share This Event
+          </a>
+        </div>
+        <div className="tp-event-details-social text-center">
+          <SocialLinks />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/event/event-area-two.tsx
+++ src/components/event/event-area-two.tsx
@@ -2,45 +2,57 @@ import Image from "next/image";
 import location_svg from "@/assets/img/icon/location.svg";
 import Link from "next/link";
 
-export default function EventAreaTwo({events}:any) {
- 
+interface EventAcf {
+  title: string;
+  image: string;
+  date: string;
+  location: string;
+}
+
+interface Event {
+  id: string | number;
+  acf: EventAcf;
+}
+
+interface EventAreaTwoProps {
+  events: Event[];
+}
+
+export default function EventAreaTwo({ events }: EventAreaTwoProps) {
   return (
     <section className="event-area pt-90 pb-70">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-lg-6">
-           
-          </div>
+          <div className="col-lg-6"></div>
         </div>
         <div className="row">
-          {events.map((item:any, i:any) => (
+          {events.map((item, i) => (
             <div key={i} className="col-lg-4 col-md-6">
               <div
                 className="tp-event-3-item mb-30 wow fadeInUp"
                 data-wow-delay={`.${i + 1}s`}
               >
                 <div className="tp-event-3-thumb">
-                  <Link href={`/event-details/${item?.acf?.title}`}>
+                  <Link href={`/event-details/${item.acf.title}`}>
                     <img
-                     
-                      src={item?.acf?.image}
-                      alt={item?.acf?.title}
+                      src={item.acf.image}
+                      alt={item.acf.title}
                       width={322}
                       height={229}
                       style={{ height: "auto" }}
                     />
                   </Link>
                   <div className="tp-event-3-date">
-                    <span>{item?.acf?.date.split('/')[0]}</span>
-                    <p>{item?.acf?.date.split('/')[1]}</p>
+                    <span>{item.acf.date.split("/")[0]}</span>
+                    <p>{item.acf.date.split("/")[1]}</p>
                   </div>
                 </div>
                 <div className="tp-event-3-content">
                   <h3 className="tp-event-3-title">
-                    <Link href={`/event-details/${item.id}`}>{item?.acf?.title}</Link>
+                    <Link href={`/event-details/${item.id}`}>{item.acf.title}</Link>
                   </h3>
                   <span>
-                    <Image src={location_svg} alt="location" /> {item?.acf?.location}
+                    <Image src={location_svg} alt="location" /> {item.acf.location}
                   </span>
                 </div>
               </div>
